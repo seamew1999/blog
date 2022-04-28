8e93d890041ca69b720023c6e6bdb0e629f7c7a1
seamew <17782876889@163.com>
2022-04-28 12:15:46 +0800

    增加了底部信息

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="footer">seamew 版权所有 陕ICP备2022004945号</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    ParentLayout
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+}
+</style>
