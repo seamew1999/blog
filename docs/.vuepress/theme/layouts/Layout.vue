516b497d774f3075c72ab8e466595cc046381c0c
seamew <17782876889@163.com>
2022-06-22 15:15:48 +0800

    修改了备案号

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,21 +1,26 @@
 <template>
   <ParentLayout>
     <template #page-bottom>
-      <div class="footer">@2022-2023 seamew 版权所有 <a target="_blank" href="https://beian.miit.gov.cn">陕ICP备2022004945号</a></div>
+      <div class="footer">
+        @2022-2023 seamew 版权所有
+        <a target="_blank" href="https://beian.miit.gov.cn">
+          陕ICP备2022004945号-1</a
+        >
+      </div>
     </template>
   </ParentLayout>
 </template>
 
 <script>
-import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import ParentLayout from "@parent-theme/layouts/Layout.vue";
 
 export default {
-  name: 'Layout',
+  name: "Layout",
 
   components: {
-    ParentLayout
-  }
-}
+    ParentLayout,
+  },
+};
 </script>
 
 <style scoped>
