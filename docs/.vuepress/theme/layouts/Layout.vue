28fbbbebaab90446eb8d067383af50e9981a5b3f
seamew <17782876889@163.com>
2023-02-28 23:05:20 +0800

    增加了公安的备案

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,8 +2,16 @@
   <ParentLayout>
     <template #page-bottom>
       <div class="footer">
-        @2022-2023 seamew 版权所有
         <a target="_blank" href="https://beian.miit.gov.cn">陕ICP备2022004945号-1</a>
+        <div class="interval">|</div>
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=61011302001621">
+          <img src="../../public/assets/img/filing.png" style="float: left" />
+          <p style="float: left; height: 20px; line-height: 20px; margin: 0px 0px 0px 5px; color: #939393">
+            陕公网安备 61011302001621号
+          </p>
+        </a>
+        <div class="interval">|</div>
+        <a href="mailto:sunhaobo@stu.xidian.edu.cn">@2022-2023 seamew 版权所有</a>
       </div>
     </template>
   </ParentLayout>
@@ -16,13 +24,18 @@ export default {
   name: "Layout",
 
   components: {
-    ParentLayout,
-  },
+    ParentLayout
+  }
 };
 </script>
 
 <style scoped>
 .footer {
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.interval {
+  margin: 0 20px;
 }
 </style>
